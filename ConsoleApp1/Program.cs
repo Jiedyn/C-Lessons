58c0063620e8fcdc0eda2b4c982c1c7c9cea0e58
Jie <Masterjiedyn@gmail.com>
2025-01-13 14:52:48 +1100

    Completed lesson 1 Brackeys C#

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "C# Lesson 1";
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WindowHeight = 40;
+
+            Console.WriteLine("You there!\nWhats your Name?");
+            Console.ReadLine();
+
+            Console.WriteLine("Its great to have you onboard cadet, You ready for your first mission?");
+            Console.ReadLine();
+
+            Console.WriteLine("Great! Meet me here at first light,");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Dismissed");
+
+
+            Console.ReadKey();
+        }
+    }
+}
