8e083b98093d94ba19d19e737431bfe7c56f0ec3
Jie <Masterjiedyn@gmail.com>
2025-01-13 16:06:12 +1100

    started E02 Brackeys C# - Started an if statement

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,11 +11,14 @@ namespace ConsoleApp1
             Console.WindowHeight = 40;
 
             Console.WriteLine("You there!\nWhats your Name?");
-            Console.ReadLine();
 
-            Console.WriteLine("Its great to have you onboard cadet, You ready for your first mission?");
-            Console.ReadLine();
+            string userName = Console.ReadLine();
 
+            Console.WriteLine("Its great to have you onboard " + userName + ", You ready for your first mission?");
+            string no = Console.ReadLine();
+            
+            if (string no )
+            Console.WriteLine("Too Bad! Meet me here at first light");
             Console.WriteLine("Great! Meet me here at first light,");
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("Dismissed");
